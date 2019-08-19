b7b38452c4795d779d8dd02b14ce8719ea4e4a2f
VmsComp <vms.comp18@gmail.com>
2019-08-19 11:07:46 -0300

    TensaoNosAlteracao

--- Código do Problema/src/questao2num/TensaoNos.java
+++ Código do Problema/src/questao2num/TensaoNos.java
@@ -18,7 +18,7 @@ public class TensaoNos {
     static float matrizNo[][];
     static float DiferencaMax;
 
-    static float calculaMaximo() {
+    static float calculaMaximo(int qtdNo) {
         int i;
         float d, m = 0;
         for (i = 1; i < qtdNo - 1; i++) {
@@ -30,16 +30,16 @@ public class TensaoNos {
         return m;
     }
 
-    static void Imprime(float[] X, int K, int varStop) {
+    static void Imprime(float[] X, int K, int varStop, int qtdNo) {
         int I;
         if (varStop == 0) {
-            if (criterioLinhas() == 1) {
+            if (criterioLinhas(qtdNo) == 1) {
                 System.out.print("Critério de Linhas: Aceito!");
             } else {
                 System.out.print("Critério de Linhas: Não aceito!");
             }
             System.out.print("\n");
-            if (criterioSa() == 1) {
+            if (criterioSa(qtdNo) == 1) {
                 System.out.print("Critério de Sassenfeld: Aceito!");
             } else {
                 System.out.print("Critério de Sassenfeld: Não aceito!");
@@ -49,13 +49,13 @@ public class TensaoNos {
                 System.out.println("Tensão no nó (" + I + ") = " + X[I] + " V\n");
             }
             if (K > 0) {
-                System.out.println("\n\nIteracao " + K + " com diferença máxima " + calculaMaximo() + "\n\n ");
+                System.out.println("\n\nIteracao " + K + " com diferença máxima " + calculaMaximo(qtdNo) + "\n\n ");
             }
 
         }
     }
 
-    static void iteracao(int k) {
+    static void iteracao(int k, int qtdNo) {
         int i, j;
 
         for (i = 1; i < qtdNo - 1; i++) {
@@ -70,10 +70,10 @@ public class TensaoNos {
             }
             X1[i] /= matrizNo[i][i];
         }
-        Imprime(X1, k + 1, 1);
+        Imprime(X1, k + 1, 1, qtdNo);
     }
 
-    static int criterioLinhas() {
+    static int criterioLinhas(int qtdNo) {
         int i, j;
         double s;
 
@@ -92,10 +92,10 @@ public class TensaoNos {
 
     }
 
-    static int criterioSa() {
+    static int criterioSa(int qtdNo) {
         int i, j;
         double s;
-        double beta[] = new double[qtdNo-1], maxB = 0.0; //
+        double beta[] = new double[qtdNo - 1], maxB = 0.0; //
 
         for (i = 1; i < qtdNo - 1; i++) {
             s = 0.0;
@@ -124,14 +124,22 @@ public class TensaoNos {
         }
     }
 
-    void principal() {
+    void principal(int qtdNo, int terra, int noFonte, int tensao, float[][] matrizNodal) {
+        int auxiliar,auxiliar2;
+        for (auxiliar = 1;  auxiliar < qtdNo-1; auxiliar++){
+            System.out.print("| "); 
+            for (auxiliar2 = 1; auxiliar2 < (2*qtdNo)+2 ; auxiliar2++){
+                System.out.print(matrizNodal[auxiliar][auxiliar2]+" ");
+            }
+            System.out.print (" |");
+                    
+            System.out.print ("\n");
+        }
         int i, j;
         int inf, inf2;
         double inf3;
         //double matrizNo[][],vetorNo[];
         Scanner in = new Scanner(System.in);
-        System.out.print("Quantidade de nós no circuito: ");
-        qtdNo = in.nextInt();
         matrizNo = new float[qtdNo - 1][qtdNo - 1];
         vetorNo = new float[qtdNo - 1];
         for (i = 1; i < qtdNo - 1; i++) {
@@ -139,82 +147,69 @@ public class TensaoNos {
                 matrizNo[i][j] = 0;
             }
         }
-        int terra, noFonte, tensao;
-        //in.nextLine();
-        System.out.print("Escolha um nó referencial (terra) (0-" + (qtdNo - 1) + "): ");
-        terra = in.nextInt();
-        in.nextLine();
-        while (terra < 0 || terra > qtdNo - 1) {
-            System.out.print("ERRO: O nó " + terra + " não está dentro do intervalo!");
-            System.out.print("\nEscolha um nó referencial (terra) (0-" + (qtdNo - 1) + "): ");
-            terra = in.nextInt();
-        }
-        System.out.print("\nQual nó recebe tensão? (0-" + (qtdNo - 1) + "): ");
-        noFonte = in.nextInt();
-        in.nextLine();
-        System.out.print("\nQual a tensão no nó (" + noFonte + ")");
-        tensao = in.nextInt();
-        in.nextLine();
-        System.out.println("\n");
-        Scanner sc = new Scanner(System.in);
-        Scanner sc1 = new Scanner(System.in);
+//        while (terra < 0 || terra > qtdNo - 1) {
+//            System.out.print("ERRO: O nó " + terra + " não está dentro do intervalo!");
+//            System.out.print("\nEscolha um nó referencial (terra) (0-" + (qtdNo - 1) + "): ");
+//            terra = in.nextInt();
+//        }
+////        Scanner sc = new Scanner(System.in);
+////        Scanner sc1 = new Scanner(System.in);
         for (i = 1; i < qtdNo - 1; i++) {
             System.out.print("---------------- Nó " + i + " ----------------\n\n");
             System.out.print("O nó " + (i) + " possui quantas conexões? ");
-            inf = in.nextInt();
-            in.nextLine();
+            inf = (int) (matrizNodal[i][1]);
+            System.out.println ("inf "+inf);
             System.out.print("\n\n");
-            for (j = 0; j < inf; j++) {
-                System.out.print("Informe a conexão " + (j + 1) + " (0-" + (qtdNo - 1) + "): ");
-                inf2 = (sc.nextInt());
-                while ((inf2 < 0 || inf2 > qtdNo - 1)) {
-                    System.out.print("O nó " + inf2 + " não está dentro do intervalo!");
+            for (j = 1; j < (2*inf)+1; j++) {
+                if (j % 2 == 0) {
                     System.out.print("Informe a conexão " + (j + 1) + " (0-" + (qtdNo - 1) + "): ");
-                    inf2 = (sc.nextInt());
-                }
-                sc.nextLine();
-                System.out.print("Informe a resistência entre V" + (i) + " e V" + inf2 + ": ");
-                inf3 = (sc1.nextFloat());
-                sc1.nextLine();
-                //sc = new Scanner(System.in);
-                matrizNo[i][i] = (float) (matrizNo[i][i] + (1 / inf3));
-                if (inf2 != terra) {
-                    if (inf2 == noFonte) {
-                        vetorNo[i] = (float) (vetorNo[i] + (tensao / inf3));
-                    } else {
-                        matrizNo[i][(inf2)] = (float) ((-1) * (1 / inf3));
+                    inf2 = (int) matrizNodal[i][j];
+                    while ((inf2 < 0 || inf2 > qtdNo - 1)) {
+                        System.out.print("O nó " + inf2 + " não está dentro do intervalo!");
+                        System.out.print("Informe a conexão " + (j + 1) + " (0-" + (qtdNo - 1) + "): ");
+                        //inf2 = (sc.nextInt());
+                    }
+//                    sc.nextLine();
+                    inf3 = matrizNodal[i][j+1]; //resistencia
+//                    sc1.nextLine();
+                    matrizNo[i][i] = (float) (matrizNo[i][i] + (1 / inf3));
+                    if (inf2 != terra) {
+                        if (inf2 == noFonte) {
+                            vetorNo[i] = (float) (vetorNo[i] + (tensao / inf3));
+                        } else {
+                            matrizNo[i][(inf2)] = (float) ((-1) * (1 / inf3));
+                        }
                     }
                 }
             }
             System.out.print("--------------------------------------\n");
         }
-        //if (criterioLinhas() == 1 && criterioSa() == 1){ 
         int k = 0;
         X0 = new float[qtdNo - 1];
         X1 = new float[qtdNo - 1];
         for (i = 1; i < qtdNo - 1; i++) {
             X0[i] = vetorNo[i] / matrizNo[i][i];
         }
-        Imprime(X0, k, 1);
+        Imprime(X0, k, 1, qtdNo);
         do {
-            iteracao(k);
-            DiferencaMax = calculaMaximo();
+            iteracao(k,qtdNo);
+            DiferencaMax = calculaMaximo(qtdNo);
             for (i = 1; i < qtdNo - 1; i++) {
                 X0[i] = X1[i];
             }
             k++;
         } while (DiferencaMax > 1.0e-3);
-        Imprime(X1, k, 0);
+        Imprime(X1, k, 0,qtdNo);
 
-        System.out.println("\n\nMatriz original de dados:\n");
-        for (i = 1; i < qtdNo - 1; i++) {
-            System.out.print("|");
-            for (j = 1; j < qtdNo - 1; j++) {
-                System.out.printf("  %8.2f  ", matrizNo[i][j]);
-            }
-            System.out.printf("  |  %8.2f  |\n", vetorNo[i]);
-
-        }
+//        System.out.println("\n\nMatriz original de dados:\n");
+//        for (i = 1; i < qtdNo - 1; i++) {
+//            System.out.print("|");
+//            for (j = 1; j < qtdNo - 1; j++) {
+//                System.out.printf("  %8.2f  ", matrizNo[i][j]);
+//            }
+//            System.out.printf("  |  %8.2f  |\n", vetorNo[i]);
+//
+//        }
     }
 }
 //}
